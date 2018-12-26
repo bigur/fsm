0557f2c525c03b2fe2a8e4d795d820a447076973
Gennady Kovalev <gik@bigur.ru>
2018-12-27 00:57:36 +0300

    Рефакторинг fsm

--- bigur/fsm/test/__init__.py
+++ bigur/fsm/test/__init__.py
@@ -1,8 +1,3 @@
-'''Поддержка конечного автомата.'''
-
 __author__ = 'Gennady Kovalev <gik@bigur.ru>'
 __copyright__ = '(c) 2016-2018 Business group for development management'
 __licence__ = 'For license information see LICENSE'
-
-from .fsm import on, transition
-from .fsm import State, FSM
